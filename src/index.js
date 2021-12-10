6fb6f86fae3974f0af451fe08e331823bb06399a
maxarvid <max.arvid.anderson@gmail.com>
2021-12-10 12:39:04 +0100

    adjust axios baseURL setting for production build

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  axios.defaults.baseURL = "http://localhost:3000";
+} else {
+  axios.defaults.baseURL = "https://peaceful-escarpment-09091.herokuapp.com";
+}
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
