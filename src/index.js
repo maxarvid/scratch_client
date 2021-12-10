571c7e071c99c54391ee6ecc94760a0523bb77eb
maxarvid <max.arvid.anderson@gmail.com>
2021-12-10 12:41:03 +0100

    adds axios import to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import axios from "axios";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
 
